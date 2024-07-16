35f7c89e73f43dc55d5577178d77548fc313695c
Hritik2816 <133709349+Hritik2816@users.noreply.github.com>
2024-07-16 15:11:31 +0530

    day04

---
+++ Day04/04day.js
@@ -0,0 +1,83 @@
+// Activity 1: For Loop
+
+
+// for (let a = 0; a <= 10; a++) {
+//   console.log(a)
+// }
+
+// let num = 5
+// for (let n = 1; n <= 10; n++) {
+//   console.log(`${num} * ${n} = ${num * n}`)
+
+// }
+
+// Activity 2: While Loop
+
+// let sum = 0
+// let num = 1
+// while (num <= 10) {
+//   sum += num
+//   num++
+// }
+// console.log(sum);
+
+// let i = 10
+// while (i >= 0) {
+//   console.log(i)
+//   i--
+// }
+
+
+// Activity 3: Do-While Loop
+
+// let i = 1
+// do {
+//   console.log(i)
+//   i++
+
+// } while (i <= 5);
+
+// let result = 5
+// let number = 1
+// do {
+//   result *= number;
+//   number++
+// } while (number <= 5);
+
+// console.log(result);
+
+
+// Activity 4: Nested Loop
+
+// for (let i = 1; i <= 5; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += "* ";
+//   }
+//   console.log(row);
+// }
+
+// for (let i = 4; i >= 1; i--) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += "* ";
+//   }
+//   console.log(row);
+// }
+
+
+// Activity 4: Loop control statement
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 7) {
+//     break;
+//   }
+//   console.log(i);
+// }
